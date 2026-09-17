37cac4f2aab138eac4d446583a6d955467dd03eb
孙达明 <83948628+tyza66@users.noreply.github.com>
2026-09-18 04:55:04 +0800

    fix(auth): passwd bundle 生成 root 空密码(root::)可被无密码登录 - 改为 root:! 锁定, 与 dropbear bundle 一致(fail-closed)

--- src/minimal_overlay/bundles/passwd/01_create.sh
+++ src/minimal_overlay/bundles/passwd/01_create.sh
@@ -5,6 +5,8 @@ set -e
 . ../../common.sh
 
 mkdir -p $DEST_DIR/etc
-echo 'root::0:0:Superuser:/:/bin/sh' > $DEST_DIR/etc/passwd
+# 不预设空密码 root: 密码字段置 '!' 锁定(fail-closed), 与 dropbear bundle 一致,
+# 避免镜像内含空密码账户被无密码本地/远程登录(旧值为 'root::' 空密码)。
+echo 'root:!:0:0:Superuser:/:/bin/sh' > $DEST_DIR/etc/passwd
 
 cd $SRC_DIR
